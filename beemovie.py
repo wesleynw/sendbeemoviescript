9f30f0216fb6a285cab2edf5372af9d0b07b03b9
Wesley Weisenberger <wesleyweisenberger@Wesleys-MacBook-Pro.local>
2020-11-06 13:25:41 -0800

    fixed

--- beemovie.py
+++ beemovie.py
@@ -1,19 +1,15 @@
 import keyboard
 from time import sleep
 
-script = open('script.txt').read().split()
-delay = 0
+#script = open('script.txt').read().split()
+script = open('script.txt').read().split('.')
 
 sleep(5)
 for i in range(len(script)):
     # print(script[i])
     for j in range(len(script[i])):
         # print(script[i][j])
-        keyboard.press_and_release(script[i][j])
+        keyboard.write(script[i][j])
     sleep(0.05)
     keyboard.press_and_release('enter')
     sleep(0.25)
-    delay+=1
-    if delay>=25:
-        delay=0
-        sleep(5)
